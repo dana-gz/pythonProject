76a50309c6f63e6be700fa770d165de31028351d
danaqqq <ttt9@o2.pl>
2021-05-28 19:36:53 +0200

    13/14

---
+++ 13_unit_tests/fields.py
@@ -0,0 +1,22 @@
+def validate_values(*values):
+  for arg in values:
+    if not isinstance(arg, (int, float)):
+      raise ValueError(f'Bok musi byc wartościa liczbową {arg}')
+
+
+def rectangle(a, b):
+  validate_values(a, b)
+  return a * b
+
+
+def triangle(a, h):
+  validate_values(a, h)
+  return 0.5 * a * h
+
+
+
+def trapezoid(a, b, h):
+  validate_values(a, b, h)
+  return 0.5 * (a + b) * h
+
+
